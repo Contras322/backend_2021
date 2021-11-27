800da5c0ad9fbaba6e08d5eccb491f9b97252483
Nikita Kutepov <contras322@mail.ru>
2021-11-27 15:09:51 +0300

    Init commit

--- genres/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class GenresConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'genres'
-    verbose_name = "Жанры"
--- users/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    location = models.CharField('Город', max_length=45, null=True)
-    registration_date = models.DateField('Дата регистрации', null=True)
